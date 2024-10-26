7cb295970f0f43d2582ec1409edf4e7fa9bf74ad
nikitaivanov <iwaa0303@mail.ru>
2024-10-26 14:22:36 +0300

    Initial commit

---
+++ src/test/kotlin/org/example/notioncodeexec/NotionCodeExecApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.notioncodeexec
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotionCodeExecApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
